3d59eb00653276a5a840724693f653e599a7e9b0
Eliot996 <math10t6@stud.kea.dk>
2021-11-17 11:31:54 +0100

    added test file

---
+++ test/AcountTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AcountTest {
+
+    @Test
+    void insert() {
+        new Acount(1, "guler", 0);
+    }
+}
