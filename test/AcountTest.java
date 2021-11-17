f89a587f7e075fbf093a6a076f9de86a0590cdd0
Eliot996 <math10t6@stud.kea.dk>
2021-11-17 11:40:23 +0100

    added tests to AccountTest

--- test/AcountTest.java
+++ test/AcountTest.java
@@ -5,7 +5,41 @@ import static org.junit.jupiter.api.Assertions.*;
 class AcountTest {
 
     @Test
-    void insert() {
-        new Acount(1, "guler", 0);
+    void testInsert_PositiveNumber() {
+        // Arrange
+        Account account = new Account(1, "guler", 0);
+        int toAdd = 10;
+        int expected = 10;
+
+        // Act
+        int result = account.insert(toAdd);
+
+        // Assert
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void testInsert_PositiveNumber_WithPreexistingBalance() {
+        // Arrange
+        Account account = new Account(1, "guler", 100);
+        int toAdd = 10;
+        int expected = 110;
+
+        // Act
+        int result = account.insert(toAdd);
+
+        // Assert
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void testInsert_NegativeNumber() {
+        // Arrange
+        Account account = new Account(1, "guler", 0);
+        int toAdd = -10;
+
+        // Act & Assert
+        assertThrows(IllegalArgumentException.class,
+                () -> account.insert(toAdd));
     }
 }
