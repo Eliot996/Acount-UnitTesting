768fce77c7333d83e546ff9754e503934803f1bf
Eliot996 <math10t6@stud.kea.dk>
2021-11-18 08:46:34 +0100

    added transfer function

--- src/Account.java
+++ src/Account.java
@@ -1,6 +1,7 @@
 public class Account {
+    private final int TRANSFER_FEE = 5;
     private final int accountNumber;
-    private final  String clientName;
+    private final String clientName;
     private int balance = 0;
 
     public Account(int acountNumber, String clientName) {
@@ -41,10 +42,41 @@ public class Account {
         return balance;
     }
 
+    public int transfer(int amount, Account target){
+        if (amount < 0) {
+            throw new IllegalArgumentException("Cannot transfer negativ numbers from an account");
+        } else if (amount + TRANSFER_FEE > balance) {
+            throw new TooLowBalanceException("The account balance is to low");
+        } else if (target != null) {
+            throw new IllegalArgumentException("Cannot transfer to an nonexsisting account");
+        }
+
+        // subtract
+        balance -= amount + TRANSFER_FEE;
+
+        // add to target
+        target.insert(amount);
+
+        // return the new balance
+        return balance;
+    }
+
     @Override
     public String toString() {
         return "Account number: " + accountNumber + '\n' +
                 "Client Name: '" + clientName + '\n' +
                 "Balance: " + balance;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getClientName() {
+        return clientName;
+    }
+
+    public int getBalance() {
+        return balance;
     }
 }
